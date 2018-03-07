732b3e5f87ed2faaab21b5968930869682737221
Norrisanator <matthew.norris96@gmail.com>
2018-03-07 22:58:10 +1300

    created ticTacToe.py

---
+++ ticTacToe.py
@@ -0,0 +1,11 @@
+the_board = {"TOP_LEFT": " ", "TOP_MID": " ", "TOP_RIGHT": " ",
+             "MID_LEFT": " ", "MID_MID": " ", "MID_RIGHT": " ",
+             "BOT_LEFT": " ", "BOT_MID": " ", "BOT_RIGHT": " "}
+
+
+def print_board():
+    print(the_board["TOP_LEFT"] + "|" + the_board["TOP_MID"] + "|" + the_board["TOP_RIGHT"])
+    print("-+-+-")
+    print(the_board["MID_LEFT"] + "|" + the_board["MID_MID"] + "|" + the_board["MID_RIGHT"])
+    print("-+-+-")
+    print(the_board["BOT_LEFT"] + "|" + the_board["BOT_MID"] + "|" + the_board["BOT_RIGHT"])
